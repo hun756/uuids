ae75517e3f24fa5f8e55bc18bd6df7cd3f33b7ab
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-08 18:48:13 +0300

    Refactor example code to remove commented-out sections and streamline UUID generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
-#include <cstdio>
-#include <iostream>
 #include "uuids/uuidv4.hpp"
+#include <iostream>
 
 int main()
 {
-    // (void)fprintf(stdout, "Hello from the uuids library!\n");
-    // uuids::uuid_generator generator;
-    // for (int i = 0; i < 100; i++)
-    // {
-    //     auto uuid = generator();
-    //     std::cout << "Generated UUID: " << uuid << std::endl;
-    // }
+    uuids::uuid_generator generator;
+    for (int i = 0; i < 100; i++)
+    {
+        auto uuid = generator();
+        std::cout << "Generated UUID: " << uuid << std::endl;
+    }
     return 0;
 }
